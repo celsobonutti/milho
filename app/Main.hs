f7e45c6e21655951053e223d9b18f30389fce475
Celso Bonutti <i.am@cel.so>
2021-10-14 13:56:49 -0300

    feat: adds repl and run subcommands

--- app/Main.hs
+++ app/Main.hs
@@ -3,24 +3,24 @@
 module Main where
 
 import           Canjica.EvalApply
+import           Capability.Error
 import           Capability.Reader
 import           Capability.State
-import           Capability.Error
 import           Data.FileEmbed
 import           Data.IORef
-import           Data.Text (strip)
 import qualified Data.Map                      as Map
-import           Pipoquinha.Error (T (ParserError))
-import qualified Pipoquinha.Parser as Parser
-import           Pipoquinha.Environment ( StateCapable
-                                        , ReaderCapable
-                                        , CatchCapable
-                                        )
-import qualified Pipoquinha.Environment as Environment
-import qualified Pipoquinha.SExp as SExp
-import           Pipoquinha.SExp (T (Error)) 
+import           Data.Text                      ( strip )
+import           Pipoquinha.Environment         ( CatchCapable
+                                                , ReaderCapable
+                                                , StateCapable
+                                                )
+import qualified Pipoquinha.Environment        as Environment
+import           Pipoquinha.Error               ( T(ParserError) )
+import qualified Pipoquinha.Parser             as Parser
+import qualified Pipoquinha.SExp               as SExp
+import           Pipoquinha.SExp                ( T(Error) )
 import           Prelude                        ( IO )
-import           Protolude                      hiding (catch)
+import           Protolude               hiding ( catch )
 import           System.IO                      ( BufferMode(NoBuffering)
                                                 , hSetBuffering
                                                 , stdout
@@ -35,32 +35,34 @@ main = do
   hSetBuffering stdout NoBuffering
   case parseFile basicOps of
     Left  e        -> putStrLn e
-    Right builtIns -> do 
-      let
-        table = Environment.Table { variables = Map.empty
-                                  , parent = Nothing
-                                  }
+    Right builtIns -> do
+      let table = Environment.Table { variables = Map.empty, parent = Nothing }
       tableRef <- newIORef table
       let environment = Environment.T { table = tableRef }
-      putStrLn ("Welcome to the 🌽 REPL!" :: Text)
       mapM_ (\atom -> Environment.runM (eval atom) environment) builtIns
-      forever $ Environment.runM run environment
+      args <- getArgs
+      case args of
+        ["repl"] -> do
+          putStrLn ("Welcome to the 🌽 REPL!" :: Text)
+          forever $ Environment.runM run environment
+        ["run", path] -> do
+          putStrLn ("Not implemented yet" :: Text)
+      
 
-run
-  :: (StateCapable SExp.T m, ReaderCapable SExp.T m, CatchCapable m)
-  => m ()
+run :: (StateCapable SExp.T m, ReaderCapable SExp.T m, CatchCapable m) => m ()
 run = do
   liftIO $ putStr ("🌽> " :: Text)
   input <- liftIO getLine
   case parse Parser.sExpLine mempty input of
-    Left  bundle -> print . Error . ParserError . toS . errorBundlePretty $ bundle
-    Right atom   -> do
-      atom <- catch @"runtimeError"
-              (eval atom)
-              (return . Error)
+    Left bundle ->
+      print . Error . ParserError . toS . errorBundlePretty $ bundle
+    Right atom -> do
+      atom <- catch @"runtimeError" (eval atom) (return . Error)
       print atom
 
 parseFile :: ByteString -> Either Text [SExp.T]
 parseFile =
-  first (toS . errorBundlePretty) . parse Parser.sExpFile mempty . strip . decodeUtf8
-  
+  first (toS . errorBundlePretty)
+    . parse Parser.sExpFile mempty
+    . strip
+    . decodeUtf8
